22b8bee2cf09b173af0dccfa3857007c5920d9fd
SangKwon93 <apfhda7@gmail.com>
2022-09-16 18:37:47 +0900

    python10_finished

---
+++ python_bootcamp_udemy/calculator.py
@@ -0,0 +1,51 @@
+## 계산기
+
+
+from replit import clear
+from art import logo
+
+  # 계산 함수 만들기
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+"+": add,
+"-": subtract,
+"*": multiply,
+"/": divide
+}
+
+  # 계산기 함수 
+def calculator():
+    print(logo)
+
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+      print(symbol)
+    should_continue = True
+   
+   # 연속적으로 사칙연산 활용
+    while should_continue:
+      operation_symbol = input("Pick an operation: ")
+      num2 = float(input("What's the next number?: "))
+      calculation_function = operations[operation_symbol]
+      answer = calculation_function(num1, num2)
+      print(f"{num1} {operation_symbol} {num2} = {answer}")
+  # 진행 여부에 따른 경우마다 결과물 제시
+      if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+        num1 = answer
+      else:
+        should_continue = False
+        clear()
+        calculator()
+
+calculator()
